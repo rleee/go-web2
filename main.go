a4fd1ba934752db890d3d195ee172d01f65f73aa
rleee <danlee0150@gmail.com>
2020-02-25 16:01:16 +0800

    super basic web server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello there</h1>")
+}
 
 func main() {
-	fmt.Println("Done")
+	http.HandleFunc("/", handlerFunc)
+
+	fmt.Println("http://localhost:8000")
+	http.ListenAndServe(":8000", nil)
 }
